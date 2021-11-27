e0aeb8b59b012563d90df36858414e7f17bf6086
DefectingCat <i@defect.ink>
2021-11-27 22:35:55 +0800

    Modify doc folder

---
+++ doc/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/xfy/Git/rust-playground/doc/node_modules/infima/dist/css/default/default.css"),
+  require("/home/xfy/Git/rust-playground/doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/xfy/Git/rust-playground/doc/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/home/xfy/Git/rust-playground/doc/src/css/custom.css"),
+];
