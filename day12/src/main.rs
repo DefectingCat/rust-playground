c48222f71bbbbb3807e9cee3d109085b910a0a99
Defectink <i@defect.ink>
2021-12-11 00:58:12 +0800

    Update lifetime

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,17 +1,34 @@
-fn main() {
-    let s1 = String::from("xfy");
-    let result;
+use std::fmt::Display;
 
-    {
-        let s2 = String::from("Hello world!");
-        result = longest(&s1[..], &s2[..]);
-        println!("{}", result)
-    }
-
-    // println!("{}", result)
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+fn main() {
+    let novel = String::from("Call me xfy. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    let s1 = String::from("hello");
+    let s2 = &s1;
+
+    println!("{}", longest_with_an_announcement(&s1[..], &s2[..], "Test"))
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, an: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", an);
     if x.len() > y.len() {
         x
     } else {
