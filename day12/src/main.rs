32e3f4811757aa61d1ba32f3e8efc6655a129c67
DefectingCat <i@defect.ink>
2021-12-10 17:44:29 +0800

    Update lifetime

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,12 +1,20 @@
-use std::fmt::{Debug, Display};
 fn main() {
-    let mut arr = vec![1, 92, 3, 90, 321, 26];
+    let s1 = String::from("xfy");
+    let result;
 
-    check_arr(&arr);
-    arr.push(42);
-    check_arr(&arr);
+    {
+        let s2 = String::from("Hello world!");
+        result = longest(&s1[..], &s2[..]);
+        println!("{}", result)
+    }
+
+    // println!("{}", result)
 }
 
-fn check_arr<T: Display + Debug>(list: &[T]) {
-    println!("The arr length is {:?} and arr is {:?}", list.len(), list)
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
